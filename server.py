7e7525e02ac37637c19e78711d3751a50d2543ee
majkelEXE <michal.dubrowski@wp.pl>
2022-04-14 20:22:08 +0200

    simpleServerChangesAndDesktopAppDevelopment

--- server.py
+++ server.py
@@ -2,28 +2,32 @@ from logging import exception
 from flask import Flask, send_from_directory, request, jsonify
 import random
 import pymongo
-
+import json
 
 app = Flask(__name__)
-
-"""
-TODO Connect MongoDB
-Handle users
-Save configuration as one big JSON
-Figure out how to use GridFS to save images and what format of files you will need
-"""
 
 # Path for our main Svelte page
 @app.route("/")
 def base():
+
+    initPageConfiguration = open("Client/public/initPageConfiguration.json")
+    data = json.load(initPageConfiguration)
+
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     db = myclient["cms"]
-    db_collection_users = db["users"]
-    
+    db_collection_users = db["users"]    
+    db_collection_pageConfiguration = db["pageConfiguration"]
+
     try:
         db_collection_users.insert_one({ "_id": "admin", "password": "admin", "permission": "admin"})
     except Exception as exception:
         print("Admin already exists")
+
+    try:
+        db_collection_pageConfiguration.insert_one({"_id": "pageConfigurationSettings", "configuration": data})
+    except Exception as exception:
+        print("Page configuration is already set")
+
 
     return send_from_directory('Client/public', 'index.html')
 
@@ -36,20 +40,14 @@ def home(path):
 def hello():
     return str(random.randint(0, 100))
 
-"""
-TODO Handle Register
-Validate
-user = { email, password, permission: "admin | permitted | user" };
-Check if user not exist else return(string) errorMessage
-Add user to db 
-Return user data
-"""
 
 #dfddddddddddddd
 
 @app.route("/register", methods=["POST"])
 def register():
     data = request.get_json()
+
+    print(data)
 
     myclient = pymongo.MongoClient("mongodb://localhost:27017/")
     db = myclient["cms"]
@@ -96,15 +94,36 @@ def login():
             'errorMessage': "Wrong password or email"
         })
 
-
-
-
 """
-TODO Handle Login
-Validate
-user = { email, password };
-Return If ok user data else (string) errorMessage
+TODO Connect MongoDB
+Handle users
+Save configuration as one big JSON
+Figure out how to use GridFS to save images and what format of files you will need
 """
+
+@app.route("/saveConfiguration", methods=["POST"])
+def saveConfiguration():
+    data = request.get_json()
+
+    myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+    db = myclient["cms"]
+    db_collection_pageConfiguration = db["pageConfiguration"]
+
+    try:
+        db_collection_pageConfiguration.update_one({"_id": "pageConfigurationSettings"}, {"configuration": data})
+    except Exception as exception:
+        print("Page configuration is already set")
+
+    try:
+        db_collection_pageConfiguration.insert_one(data)
+        return jsonify({
+            'message': "Configuration is saved!"
+        })
+    except Exception as exception:
+        #assert type(exception).__name__ == 'NameError'
+        return jsonify({
+            'errorMessage': "Cant save configuration"
+        })
 
 """
 TODO Send users
